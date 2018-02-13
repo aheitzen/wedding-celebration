5b93edcec1985640f6f7e538eab9c632dfad04e0
Alexa Heitzenrader <aheitzen1@gmail.com>
2018-02-12 17:47:48 -0800

    what

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
-$(document).ready(function() {
-	$('#fullpage').fullpage();
-});
+// $(document).ready(function() {
+// 	$('#fullpage').fullpage();
+// });
 
 $(document).ready(function (){
+	$("#home").click(function (){
+    	$('html, body').animate({
+        	scrollTop: $("#hero-section").offset().top
+        }, 1000);
+  	});
   	$("#details-pull").click(function (){
     	$('html, body').animate({
         	scrollTop: $("#info-section").offset().top
