9d1a91391d27e3f5a107579c612ad751b7c7987c
Kendrick Parks <parks.kendrick@gmail.com>
2018-02-14 22:06:24 -0800

    adding minor changes to port fallback and package.json

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ var app = express();
 
 app.use('/', express.static('app'));
 
-app.listen(process.env.PORT, function () {
+app.listen(process.env.PORT || 8080, function () {
 	console.log('listening on port 8080');
 })
